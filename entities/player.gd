29a142325b9f047c0a474ec7706b429d75e81670
Oskar van Velden <oskar@rakso.nl>
2021-04-11 12:12:29 +0200

    Add player movement

--- entities/player.gd
+++ entities/player.gd
@@ -1,16 +1,33 @@
 
-extends Sprite
+extends KinematicBody2D
 
-func _ready():
-	pass
+var velocity: Vector2 = Vector2.ZERO
+var MAX_SPEED = 210
+var ACCELERATION = 1000
+var FRICTION = 600
+
+var target_position: Vector2 = Vector2.ZERO
 	
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.is_action_pressed("ui_left"):
-			position.x -= 10
-		elif event.is_action_pressed("ui_right"):
-			position.x += 10
-		elif event.is_action_pressed("ui_down"):
-			position.y += 10
-		elif event.is_action_pressed("ui_up"):
-			position.y -= 10
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		input_vector = get_viewport().get_mouse_position() - position
+		input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		apply_movement(input_vector * ACCELERATION * delta)
+	else:
+		apply_friction(FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
+	
+func apply_movement(acceleration):
+	velocity += acceleration
+	velocity = velocity.clamped(MAX_SPEED)
+	
+func apply_friction(friction):
+	if velocity.length() > friction:
+		velocity -= velocity.normalized() * friction
+	else:
+		velocity = Vector2.ZERO
