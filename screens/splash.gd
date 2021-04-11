65fbfeafaa18df785021c75bb6fa714cfcc43215
Oskar van Velden <oskar@rakso.nl>
2021-04-11 23:53:53 +0200

    Enable splash

--- screens/splash.gd
+++ screens/splash.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	yield(get_tree().create_timer(3.0), "timeout")
+	yield(get_tree().create_timer(1.0), "timeout")
 	
-#	Game.transition_to(Game.GameState.MAIN_MENU)
-	Game.start_game()
+	Game.transition_to(Game.GameState.MAIN_MENU)
