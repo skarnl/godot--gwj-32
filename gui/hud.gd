9e7e166fa9397a82ef545d672b5ff2552551bb52
Oskar van Velden <oskar@rakso.nl>
2021-04-11 23:52:46 +0200

    Track score in level

--- gui/hud.gd
+++ gui/hud.gd
@@ -1,45 +1,32 @@
 extends CanvasLayer
 
-signal won
-
-
-var score = 0
-
-
 func _ready() -> void:
-	$WonLabel.hide()
-	$WonExplosion.hide()
+	$Won.hide()
 	$DiedMessage.hide()
 	$Score.show()
+	
+	reset_score()
+
+
+func update_score(score: int) -> void:
+	$Score.text = str(score)
+	$Explode.restart()
 
 
 func reset_score() -> void:
-	score = 0
-	update_score()
-	
-
-func increase_score() -> void:
-	score += 1
-	$Explode.restart()
-	update_score()
-	
-	if score == 7:
-		emit_signal('won')
-		show_won()
-	
-
-func update_score() -> void:
-	$Score.text = str(score)
+	$Score.text = str(0)
 
 
 func show_died() -> void:
 	$DiedMessage.show()
-	$Score.hide()
-
+	
+	
+func hide_died() -> void:
+	$DiedMessage.hide()
+	
 
 func show_won() -> void:
 	$DiedMessage.hide()
 	$Score.hide()
-	$WonLabel.show()
-	$WonExplosion.show()
-	$WonExplosion.emitting = true
+	$Won/WonExplosion.emitting = true
+	$Won.show()
--- screens/game/level01.gd
+++ screens/game/level01.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+
+var score = 0
+
+
 func _ready():
-	$HUD.reset_score()
-	
 	$Diamond.connect('pickup', self, 'on_diamond_pickup')
 	$Diamond2.connect('pickup', self, 'on_diamond_pickup')
 	$Diamond3.connect('pickup', self, 'on_diamond_pickup')
@@ -10,8 +12,24 @@ func _ready():
 	$Diamond5.connect('pickup', self, 'on_diamond_pickup')
 	$Diamond6.connect('pickup', self, 'on_diamond_pickup')
 	$Diamond7.connect('pickup', self, 'on_diamond_pickup')
+	
+	$HUD.connect('continue', self, 'on_press_continue')
+	
+	set_process_input(false)
 
 func on_diamond_pickup() -> void:
 	print('player pick diamond - increase score')
 
-	$HUD.increase_score()
+	_increase_score()
+
+func _increase_score() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+	if score == 7:
+		$HUD.show_won()
+		set_process_input(true)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		Game.goto_next_level()
